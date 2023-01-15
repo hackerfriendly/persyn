f12f7834165dcd2862f894e4f3d5a18af52dfe44
Rob Flickenger <rob@hackerfriendly.com>
2023-01-15 12:36:55 -0800

    Chunk text on import

--- src/chat/stdio/import_text.py
+++ src/chat/stdio/import_text.py
@@ -12,12 +12,28 @@ import argparse
 import os
 import re
 
+# Color logging
+from utils.color_logging import log
+
 # Bot config
 from utils.config import load_config
 
 from interaction.interact import Interact
 
-import fileinput
+# Defined in main
+interact = None
+
+def save_graph(channel, convo_id, text, include_archetypes):
+    ''' Save the relationship graph '''
+    log.debug('👉', text)
+
+    return interact.recall.ltm.save_relationship_graph(
+        service='import_service',
+        channel=channel,
+        convo_id=convo_id,
+        text=text,
+        include_archetypes=include_archetypes
+    )
 
 def main():
     ''' Main event '''
@@ -39,29 +55,34 @@ def main():
         help='Convert entities to archetypes (default: False)',
         default=False
     )
-    parser.add_argument('files', nargs='?', type=str, help='One or more text files to import')
+    parser.add_argument('files', nargs='+', help='One or more text files to import')
 
     args = parser.parse_args()
 
     persyn_config = load_config(args.config)
+    global interact
     interact = Interact(persyn_config)
 
-    with fileinput.FileInput(files=args.files, mode='r') as f:
-        for line in f:
-            line = line.strip()
-            if not line or not re.search('[a-zA-Z]', line):
-                continue
+    channel = f"{args.author.replace('|', '_')}|{args.title}"
 
-            print(line)
+    for file in args.files:
+        with open(file, mode='r', encoding='utf-8') as f:
+            lines = []
+            for line in f:
+                line = line.strip()
+                if not line or not re.search('[a-zA-Z]', line):
+                    continue
 
-            assert interact.recall.ltm.save_relationship_graph(
-                    service='import_service',
-                    channel=f"{args.author.replace('|', '_')}|{args.title}",
-                    convo_id=args.convo_id,
-                    text=line,
-                    include_archetypes=False
-            ) == 'created'
+                if len(' '.join(lines)) + len(line) > interact.completion.max_prompt_length:
+                    assert save_graph(channel, args.convo_id, ' '.join(lines), args.archetypes) == "created"
+                    lines = [line]
+                else:
+                    lines.append(line)
 
+            if lines:
+                assert save_graph(channel, args.convo_id, ' '.join(lines), args.archetypes) == "created"
+
+        log.info(f"📚 imported: {file}")
 
 if __name__ == '__main__':
     main()
