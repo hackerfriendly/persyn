a89ab1ce8a9682f5785f53abdcec084f2d15ac2e
Rob Flickenger <rob@hackerfriendly.com>
2023-04-30 14:07:19 -0700

    Removed redis_om

--- redis_om/model/__init__.py
+++
@@ -1,9 +0,0 @@
-from .migrations.migrator import MigrationError, Migrator
-from .model import (
-    EmbeddedJsonModel,
-    Field,
-    HashModel,
-    JsonModel,
-    NotFoundError,
-    RedisModel,
-)
