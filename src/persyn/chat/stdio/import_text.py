06e0c87db6ffa2b73f5af7896d7f845b5d2be5d9
Rob Flickenger <rob@hackerfriendly.com>
2023-06-04 22:35:35 -0700

    Save every sentence, summarize every max_prompt_length.

--- src/persyn/chat/stdio/import_text.py
+++ src/persyn/chat/stdio/import_text.py
@@ -7,7 +7,7 @@ Summarization is run through the completion model, so use with caution.
 
 Text should be preformatted to remove line breaks within paragraphs.
 """
-# pylint: disable=import-error, wrong-import-position, wrong-import-order, invalid-name, no-member
+# pylint: disable=import-error, wrong-import-position, wrong-import-order, no-member, invalid-name
 import argparse
 import os
 import re
@@ -19,20 +19,6 @@ from persyn.utils.color_logging import log
 from persyn.utils.config import load_config
 
 from persyn.interaction.interact import Interact
-
-# Defined in main
-interact = None
-
-def save_imported_convo(service, channel, convo_id, text, author, summarize=False):
-    ''' Save the imported conversation '''
-    log.debug('👉', text)
-
-    if summarize:
-        interact.recall.save(service, channel, text, author, None, verb="wrote")
-        interact.summarize_convo(service, channel, save=True, dialog_only=False)
-
-    else:
-        raise NotImplementedError('Not implemented yet.')
 
 def main():
     ''' Main event '''
@@ -47,11 +33,11 @@ def main():
     )
     parser.add_argument('--title', type=str, help='A title for this document (required)')
     parser.add_argument('--author', type=str, help='Author of this document (required)')
-    parser.add_argument('--convo_id', type=str, help='convo_id (if not specified, generate one)', default=None)
+    parser.add_argument('--model', type=str, default='gpt-3.5-turbo', help='Summarization model (default: gpt-3.5-turbo)')
     parser.add_argument(
         '--summarize',
         action='store_true',
-        help='Summarize text using completion (default: False, might cost $)',
+        help='Summarize text using completion (default: False, slow and potentially 💸)',
         default=False
     )
     parser.add_argument('files', nargs='+', help='One or more text files to import')
@@ -59,16 +45,22 @@ def main():
     args = parser.parse_args()
 
     persyn_config = load_config(args.config)
-    global interact
+
     interact = Interact(persyn_config)
 
     service = 'import_service'
     channel = f"{args.author.replace('|', '_')}|{args.title}"
 
     # Make a cup of tea and settle down in your favorite chair.
-    if args.summarize:
-        interact.recall.save(service, channel, f"{args.title} by {args.author}",
-                             persyn_config.id.name, persyn_config.id.guid, verb="reads")
+    doc = interact.recall.save_convo_line(
+        service,
+        channel,
+        f"{args.title} by {args.author}",
+        persyn_config.id.name,
+        persyn_config.id.guid,
+        verb="reads"
+    )
+    log.info("📚 New convo_id:", doc.convo_id)
 
     for file in args.files:
         with open(file, mode='r', encoding='utf-8') as f:
@@ -77,20 +69,51 @@ def main():
                 line = line.strip()
                 if not line or not re.search('[a-zA-Z]', line):
                     continue
+                lines.append(line)
 
-                convo = ' '.join(lines)
-                if interact.completion.toklen(convo + line) + 1 > interact.completion.max_prompt_length():
-                    save_imported_convo(
-                        service, channel, args.convo_id, convo, args.author, summarize=args.summarize
+        page = []
+        for sent in interact.completion.nlp(' '.join(lines)).sents:
+            log.debug(sent.text)
+
+            interact.recall.save_convo_line(
+                service,
+                channel,
+                msg=sent.text,
+                speaker_name=args.author,
+                speaker_id=None,
+                convo_id=interact.recall.convo_id(service, channel),
+                verb="wrote"
+            )
+
+            if interact.completion.toklen(' '.join(page) + sent.text) + 1 > interact.completion.max_prompt_length():
+                if args.summarize:
+                    log.debug("Maximum length reached, summarizing.")
+
+                    interact.summarize_convo(
+                        service,
+                        channel,
+                        save=True,
+                        dialog_only=False,
+                        model=args.model,
+                        save_kg=False
                     )
-                    lines = [line]
-                else:
-                    lines.append(line)
 
-            if lines:
-                save_imported_convo(service, channel, args.convo_id, convo, args.author, summarize=args.summarize)
+                page = [sent.text]
+            else:
+                page.append(sent.text)
 
-        log.info(f"📚 imported: {file}")
+        if page and args.summarize:
+            log.debug("Final summary.")
+            interact.summarize_convo(
+                service,
+                channel,
+                save=True,
+                dialog_only=False,
+                model=args.model,
+                save_kg=False
+            )
+
+        log.info(f"📚 Imported: {file}")
 
 if __name__ == '__main__':
     main()
