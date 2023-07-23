69ab1d7353e2ae76258d0c12a6bcfea61e564a3b
Rob Flickenger <rob@hackerfriendly.com>
2023-07-22 17:57:25 -0700

    Only take photos if dreams_url is defined

--- src/persyn/chat/common.py
+++ src/persyn/chat/common.py
@@ -82,7 +82,7 @@ class Chat():
         log.warning(f"∑ {reply.json()['summary']}")
 
         if summary:
-            if photo:
+            if self.dreams_url and photo:
                 self.take_a_photo(
                     channel,
                     summary,
@@ -130,7 +130,7 @@ class Chat():
 
         log.warning(f"[{channel}] {self.bot_name}:", reply)
 
-        if any(verb in reply for verb in self.photo_triggers):
+        if self.dreams_url and any(verb in reply for verb in self.photo_triggers):
             self.take_a_photo(
                 channel,
                 self.get_summary(channel, max_tokens=60),
